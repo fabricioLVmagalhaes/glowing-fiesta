b944aa395b340fb000072cbf8ad7af2e9415b7dc
Fabricio Magalhaes <fabricio.l.v.magalhaes@gmail.com>
2022-03-30 19:55:31 -0300

    Restricting Pickable Dates

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -4,18 +4,19 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
+  maxDate;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.maxDate = new Date();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
   }
 
   onSubmit(form: NgForm) {
     console.log(form);
-
   }
-
 }
