3ce4689b4d08d2709f54ad68d019d535722c99da
Fabricio Magalhaes <fabricio.l.v.magalhaes@gmail.com>
2022-04-12 21:40:15 -0300

    Storing Completed Exercises on Firestore

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -45,7 +45,7 @@ export class TrainingService {
   }
 
   completeExercise() {
-    this.exercises.push({
+    this.addDataToDatabase({
       ...this.runningExercise,
       date: new Date(),
       state: 'completed',
@@ -55,7 +55,7 @@ export class TrainingService {
   }
 
   cancelExercise(progress: number) {
-    this.exercises.push({
+    this.addDataToDatabase({
       ...this.runningExercise,
       duration: this.runningExercise.duration * (progress / 100),
       calories: this.runningExercise.calories * (progress / 100),
@@ -73,5 +73,9 @@ export class TrainingService {
 
   getExercises() {
     return this.exercises.slice();
+  }
+
+  private addDataToDatabase(exercise: Exercise) {
+    this.db.collection('finishedExercises').add(exercise);
   }
 }
